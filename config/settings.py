abaf708998ec0aa9685e5ac5d30d8bc254176b0d
yuki-ishibashi-uttc <yuki14841212@gmail.com>
2025-11-15 19:53:16 +0900

    fix: Correct CORS configuration

--- config/settings.py
+++ config/settings.py
@@ -136,4 +136,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True

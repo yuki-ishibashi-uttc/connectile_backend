82a6be989cfa182d2780e4682db2650e940b23f7
yuki-ishibashi-uttc <yuki14841212@gmail.com>
2025-11-15 19:21:33 +0900

    feat: Change points from Integer to Float

--- game/models.py
+++ game/models.py
@@ -8,13 +8,11 @@ def get_default_board():
     return [[0 for _ in range(8)] for _ in range(8)]
 
 class GameState(models.Model):
-    player1_points = models.IntegerField(default=0)
-    player2_points = models.IntegerField(default=0)
-    player3_points = models.IntegerField(default=0)
-    player4_points = models.IntegerField(default=0)
-
-    # default=list ではなく、get_default_board関数を指定
-    board_state = models.JSONField(default=get_default_board) 
+    player1_points = models.FloatField(default=0.0) # ← IntegerField を FloatField に変更
+    player2_points = models.FloatField(default=0.0) # ← IntegerField を FloatField に変更
+    player3_points = models.FloatField(default=0.0) # ← IntegerField を FloatField に変更
+    player4_points = models.FloatField(default=0.0) # ← IntegerField を FloatField に変更
+    board_state = models.JSONField(default=list)
 
     def __str__(self):
         return f"Game State (ID: {self.id})"
